a4cd8caa48b76102690644750826a279d6954ff5
Matthew <matthewseaber@outlook.com>
2025-04-23 15:11:51 +0100

    Adding shadcn theme

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link"
-import { LuTent } from "react-icons/lu"
+import { SiJetpackcompose } from "react-icons/si";
 import { Button } from "../ui/button"
 
 function Logo() {
   return (
-    <Button size='icon' asChild>
-      <Link href='/'>
-        <LuTent className='w-6 h-6' />
+    <Button size="icon" asChild>
+      <Link href="/">
+        <SiJetpackcompose className="w-6 h-6" />
       </Link>
     </Button>
-  )
+  );
 }
 
 export default Logo
