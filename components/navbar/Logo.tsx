99f04480ba1640624fdde5037dd8128a1b616cef
Matthew <matthewseaber@outlook.com>
2025-08-12 09:25:21 +0100

    Fix logo theme change bug

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,22 +1,52 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 function Logo() {
-  return (
-    <>
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  let logoPath = "/logo-dark.svg";
+
+  if (!mounted) {
+    // Renders dark mode logo while waiting for theme
+    return (
       <Link href="/">
         <span className="block w-8 h-8">
           <Image
-            src="/logo-dark.svg"
-            alt="Logo, press to go home"
+            src={logoPath}
+            alt="Logo"
             width={24}
             height={24}
-            className="hidden dark:block w-full h-full object-contain"
+            className="w-full h-full object-contain"
             style={{ display: "block" }}
           />
         </span>
       </Link>
-    </>
+    );
+  }
+
+  if (resolvedTheme === "light") {
+    logoPath = "/logo-light.svg";
+  }
+
+  return (
+    <Link href="/">
+      <span className="block w-8 h-8">
+        <Image
+          src={logoPath}
+          alt="Logo"
+          width={24}
+          height={24}
+          className="w-full h-full object-contain"
+          style={{ display: "block" }}
+        />
+      </span>
+    </Link>
   );
 }
 
