d3a2f7c97c74c4e8b94f5a9485b57d323c68f092
Matthew <matthewseaber@outlook.com>
2025-09-07 17:52:39 +0100

    Updated support page temporary text

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -3,7 +3,7 @@ function SupportPage() {
     <>
       <h2 className="text-2xl font-semibold mb-4">Support Page</h2>
     <div>
-      This page isn&apos;t ready yet. In the meantime, feel free to email us cat pictures at <a href="mailto:support@packapp.co.uk" className="text-blue-600 underline">support@packapp.co.uk</a>.
+      This page isn&apos;t ready yet. In the meantime, feel free to email any questions or concerns to <a href="mailto:support@packapp.co.uk" className="text-blue-600 underline">support@packapp.co.uk</a>.
     </div>
     </>
   );
